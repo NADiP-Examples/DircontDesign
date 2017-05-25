4d3a830abaddfda30f9c85ffefc85b0b07e68ad7
booblegum <booblegum42@gmail.com>
2017-05-26 02:58:34 +0700

    add-logic
    Добавлена логика пошагового заполнения формы

--- src/app/profile/profile-demo/profile-demo.component.ts
+++ src/app/profile/profile-demo/profile-demo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { close } from "fs";
 
-type user_data_status_type = "closed" | "opened" | "saved" | "saved_animate";
+type states = "closed" | "opened" | "saved" | "saved_animate";
 
 @Component({
   // selector: 'app-profile-demo',
@@ -14,30 +14,61 @@ export class ProfileDemoComponent implements OnInit {
   //   if (input_second_name) input_second_name.nativeElement.focus();
   // }
 
-  user_data_status:user_data_status_type = "closed";
-  constructor() { }
+  user_data_status: states = "closed";
+  company_data_status: states = "closed";
+  company_visible: boolean = false;
+  legal_status = '';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  onKeyUp(event){
-    if (event.keyCode === 32){
-      event.target.nextElementSibling?event.target.nextElementSibling.focus(): '';
+  onKeyUp(event) {
+    if (event.keyCode === 32) {
+      event.target.nextElementSibling ? event.target.nextElementSibling.focus() : '';
     }
   }
 
-  animationEnd(){
+  animationEnd() {
     console.log('transitionEnd');
-    if (this.user_data_status == "saved_animate") this.user_data_status = "saved";
+    if (this.user_data_status == "saved_animate") {
+      this.user_data_status = "saved";
+      this.company_visible = true;
+    } else if (this.company_data_status == "saved_animate") {
+      this.company_data_status = "saved"
+    }
   }
 
-  saveUserData(form){
-    console.log("Save form");
+  userDataEdit() {
+    this.user_data_status = 'opened';
+  }
+
+  saveUserData(form) {
+    // console.log("Save form");
     this.user_data_status = "saved_animate";
   }
 
-  reset(){
+  selectLegalStatus(legal_status) {
+    console.log("ls change");
+    this.legal_status = legal_status;
+    this.company_data_status = 'opened';
+  }
+
+  companyDataEdit() {
+    this.company_data_status = 'opened';
+  }
+
+  saveCompanyData(form) {
+    // console.log("Save form");
+    this.company_data_status = "saved_animate";
+  }
+
+  reset() {
     this.user_data_status = "closed";
+    this.company_data_status = "closed";
+    this.company_visible = false;
   }
 
 }
